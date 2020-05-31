1a4c70732246b1dbdcb8d49ac34f4ad475f2c199
Bruno <bruno.fernandes@engdb.com.br>
2020-05-31 18:15:34 -0300

    Ajuste no cadastro de gastos para utilizar uma categoria existente ou persistir uma nova.

--- src/main/java/br/com/gft/gastos/repository/GastoRepository.java
+++ src/main/java/br/com/gft/gastos/repository/GastoRepository.java
@@ -3,9 +3,11 @@ package br.com.gft.gastos.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gft.gastos.model.Gasto;
 
+@Repository
 public interface GastoRepository extends JpaRepository<Gasto, Long>{
 	List<Gasto> findByCliente(Long id);
 }
